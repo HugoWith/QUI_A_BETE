a12922eaf31acd27e7ac0d08239b05169c2cfa94
marin <vanthiermarin@orange.fr>
2019-11-18 15:34:46 +0100

    creation user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+marin = User.create!(
+  nickname: "jr",
+  first_name: "Marin",
+  last_name: "Vanthier",
+  email: "marin@marin.com",
+  password: "azerty")
+
+hugo = User.create!(
+  nickname: "hugoxxx",
+  first_name: "Hugo",
+  last_name: "Valla",
+  email: "hugo@hugo.com",
+  password: "azerty")
+
+alex = User.create!(
+  nickname: "dadalex",
+  first_name: "Alex",
+  last_name: "Viretti",
+  email: "alex@alex.com",
+  password: "azerty")
+
+marine = User.create!(
+  nickname: "marineX3",
+  first_name: "marine",
+  last_name: "Diot",
+  email: "marine@marine.com",
+  password: "azerty")
