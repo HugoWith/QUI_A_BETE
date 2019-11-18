c66da8fc407147c1aa2cf5d8e3cc4827120fda71
Marine Diot <marine.diot@hotmail.fr>
2019-11-18 14:54:59 +0100

    routes pour bets et groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/create'
-  get 'groups/edit'
-  get 'groups/update'
-  get 'groups/destroy'
+resources :groups do
+  resources :bets
+end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
