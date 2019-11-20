19e6ba47379a3fca2ffb2440b9ab8ab0289a9d6e
Marine Diot <marine.diot@hotmail.fr>
2019-11-20 16:59:00 +0100

    ajout dune foreign key sur le bet pour qu'il y ait un winner

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -3,6 +3,7 @@ class Bet < ApplicationRecord
   belongs_to :creator, class_name: 'User', dependent: :destroy
   belongs_to :beter, class_name: 'User', dependent: :destroy
   belongs_to :target, class_name: 'User', dependent: :destroy, optional: true
+  belongs_to :winner, class_name: 'User', dependent: :destroy, optional: true
   validates :description, presence: true
   validates :end_date, presence: true
   validates :stake, presence: true
