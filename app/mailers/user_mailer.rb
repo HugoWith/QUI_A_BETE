8213830202c12ab61be69abfc46aaed90779fde3
Marine Diot <marine.diot@hotmail.fr>
2019-11-21 17:29:58 +0100

    ajustements sur emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,28 +9,29 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Tu es prêt à beter')
   end
 
-  def invite_member
-    @user = params[:user] # Instance variable => available in view
-    mail(to: @user.email, subject: "#{current_user.first_name} t'invite à beter dans son crew !")
+  def invite_member(user, owner)
+    @user = user
+    @owner = owner # Instance variable => available in view
+    mail(to: @user.email, subject: "#{owner.first_name} t'invite à beter dans son crew !")
   end
 
-  def victime
-    @user = params[:user]
-    @group_id = params[:group_id]
-    @bet = params[:bet] # Instance variable => available in view
+  def victime(user, group, bet)
+    @user = user
+    @group_id = group.id
+    @bet = bet # Instance variable => available in view
     mail(to: @user.email, subject: "Ils t'ont victimisé !")
   end
 
-  def bet_end
-    @bet = params[:bet]
-    @group = params[:group] # Instance variable => available in view
+  def bet_end(bet, group)
+    @bet = bet
+    @group = group # Instance variable => available in view
     mail(to: @user.email, subject: "#{@bet.description} est fini ! Qui a gagné ?")
   end
 
-  def got_present
-    @user = params[:user]
-    @group = params[:group]
-    @bet = params[:bet] # Instance variable => available in view
+  def got_present(user, group, bet)
+    @user = user
+    @group = group
+    @bet = bet # Instance variable => available in view
     mail(to: @user.email, subject: "Alors, t'as reçu ton dû ?")
   end
 end
