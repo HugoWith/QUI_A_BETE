33828e54adf942be6f60c64ca10d912b0a321892
Marine Diot <marine.diot@hotmail.fr>
2019-11-21 12:40:06 +0100

    encore fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :groups do
     resources :bets
-    GET "/groups/:group_id/bets/:id"
   end
   GET "/groups/:group_id/bets/:id/winner", to: "bets#who_won", as: :define_winner
 
