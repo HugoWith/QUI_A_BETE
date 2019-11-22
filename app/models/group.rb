71186732f51725f479593cd1e63535e341aa4e02
Marine Diot <marine.diot@hotmail.fr>
2019-11-22 16:30:13 +0100

    group fixe

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,11 @@
 class Group < ApplicationRecord
   IMAGES_SAMPLES_URL = ["frog.png", "Batman.png", "fire.png", "boxing.png", "beer.png", "burger.png",
     "Cat.png", "King.png", "peach.png", "rock.png", "rocket.png", "money.png", "prout.png"]
+  has_many :bets
   has_many :usergroups, dependent: :destroy
+  has_many :users, through: :usergroups
+  has_many :invitations, dependent: :destroy
+  validates :name, presence: true
   mount_uploader :photo, PhotoUploader
   #     before_validation :test
 
