c1dbcee3afedabaacfd8c409be2ba90885004c0d
Marine Diot <marine.diot@hotmail.fr>
2019-11-23 18:38:41 +0100

    on peut voir le détail d'un pari depuis le show du groupe

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,11 +1,16 @@
 class Bet < ApplicationRecord
   belongs_to :group
-  belongs_to :creator, class_name: 'User' 
-  belongs_to :beter, class_name: 'User' 
+  belongs_to :creator, class_name: 'User'
+  belongs_to :beter, class_name: 'User'
   belongs_to :target, class_name: 'User', optional: true
   belongs_to :winner, class_name: 'User', optional: true
   validates :description, presence: true
   validates :end_date, presence: true
   validates :stake, presence: true
   validates :difficulty, presence: true, inclusion: { in: (1..10) }
+
+
+  def is_over
+    self.over = true
+  end
 end
