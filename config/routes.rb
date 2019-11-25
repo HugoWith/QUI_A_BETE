ff9c4c2fa7b0e35b47291a741853f05ec2af4a29
Alex <alexou73@gmail.com>
2019-11-25 12:48:13 +0100

    roue forune

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   authenticate :user, lambda { |u| u.email == "marine@marine.com" } do
     mount Sidekiq::Web => '/sidekiq'
   end
-<<<<<<< HEAD
+
   get 'invitation/new'
   get 'invitation/post'
   get 'usergroups/join_crew'
   get 'bets/win_wheel'
-=======
->>>>>>> fcb6ed1210c5445af5b282d68b1ce2886cb5aa86
+
 
   resources :groups do
     resources :invitations, only: [:create]
@@ -17,11 +16,10 @@ Rails.application.routes.draw do
     resources :bets
   end
   get "/groups/:group_id/bets/:id/winner", to: "bets#who_won", as: :define_winner
-<<<<<<< HEAD
   # get "/groups/:group_id/bets/:id/win_wheel",  to: "bets#wheel", as: :wheel
-=======
+
   get "/groups/:group_id/new-member", to: "usergroups#create", as: :new_member
->>>>>>> fcb6ed1210c5445af5b282d68b1ce2886cb5aa86
+
 
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
