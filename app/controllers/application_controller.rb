0e83bb113a6d65dab500cd8a847e4eb6c114c870
Hugo Valla <Hugo_valla@hotmail.fr>
2019-11-25 12:36:09 +0100

    notif only sur Firefox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
     # # [...]
-  before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
 
     def configure_permitted_parameters
@@ -13,8 +13,9 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :photo, :first_name, :last_name])
     end
 
-  def after_sign_in_path_for(resource)
-    groups_path
+
+    def after_sign_in_path_for(resource)
+      groups_path
     # return the path based on resource
   end
 end
