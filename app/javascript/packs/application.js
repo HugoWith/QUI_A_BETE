82810c14b549973e1fef52d4e578739e45de4999
Marine Diot <marine.diot@hotmail.fr>
2019-11-26 18:57:48 +0100

    install librairie tippy pour quitter le crew

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,11 @@ import { initCursorJS } from '../components/cursor';
 import { initServiceWorkerJS } from '../components/serviceWorker';
 
 import { initRoll } from '../components/roue';
+import { initTippy } from '../components/init_tippy';
 
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
@@ -20,14 +20,18 @@ application.load(definitionsFromContext(context))
 initTypedJS();
 initCursorJS();
 initServiceWorkerJS();
+initRoll();
+initTippy()
 
 // initPushmanagerJS();
 // initTriggerWebPush();
 
 // initPushPermission();
-initRoll();
 
 // application()
 // context()
 
 // import "controllers"
+
+
+
