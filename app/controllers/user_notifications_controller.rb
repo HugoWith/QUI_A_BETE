e7ae89b477108e82b791ccd13418de3b8cab47cd
Alex <alexou73@gmail.com>
2019-11-26 14:39:54 +0100

    ok

--- app/controllers/user_notifications_controller.rb
+++ app/controllers/user_notifications_controller.rb
@@ -1,5 +1,5 @@
-# class UserNotificationsController < ApplicationController
-#   skip_before_action :verify_authenticity_token
+class UserNotificationsController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
 
 
@@ -13,20 +13,20 @@
     @group.users.where.not(subscription: nil).each do |user|
 
 
-#       Webpush.payload_send(
-#         message: "DTG",
-#         endpoint: user.subscription["endpoint"],
-#         p256dh: user.subscription["keys"]["p256dh"],
-#         auth: user.subscription["keys"]["auth"],
-#         vapid: {
-#           subject: "mailto:sender@example.com",
-#           public_key: ENV['VAPID_PUBLIC_KEY'],
-#           private_key: ENV['VAPID_PRIVATE_KEY']
-#         },
-#         ssl_timeout: 5, # value for Net::HTTP#ssl_timeout=, optional
-#         open_timeout: 5, # value for Net::HTTP#open_timeout=, optional
-#         read_timeout: 5 # value for Net::HTTP#read_timeout=, optional
-#       )
-#     end
-#   end
-# end
+      Webpush.payload_send(
+        message: "DTG",
+        endpoint: user.subscription["endpoint"],
+        p256dh: user.subscription["keys"]["p256dh"],
+        auth: user.subscription["keys"]["auth"],
+        vapid: {
+          subject: "mailto:sender@example.com",
+          public_key: ENV['VAPID_PUBLIC_KEY'],
+          private_key: ENV['VAPID_PRIVATE_KEY']
+        },
+        ssl_timeout: 5, # value for Net::HTTP#ssl_timeout=, optional
+        open_timeout: 5, # value for Net::HTTP#open_timeout=, optional
+        read_timeout: 5 # value for Net::HTTP#read_timeout=, optional
+      )
+    end
+  end
+end
